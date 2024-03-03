ac888263bb7adfb2906eebeff4f3d2c1a3fa7fb0
Layoute <qingfengroot@163.com>
2024-03-03 16:08:52 +0800

    feat: optimize lint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
-  extends: ["universe/native", "universe/shared/typescript-analysis"],
+  extends: ['universe/native', 'universe/shared/typescript-analysis'],
   overrides: [
     {
-      files: ["*.ts", "*.tsx", "*.d.ts"],
+      files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
       },
     },
   ],
-};
+}
